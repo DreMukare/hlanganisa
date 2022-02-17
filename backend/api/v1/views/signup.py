77c8fe20ded936edffb0144e70d01d5700046c03
Fraol123 <fraoltesfaye.ft@gmail.com>
2022-02-17 16:51:58 +0300

    signup

---
+++ backend/api/v1/views/signup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+""" Handle RESTFul API actions for users """
+import bcrypt
+from os import path, mkdir
+from flask import current_app
+from models.user import User
+from models import storage, image_storage, redis_cache
+from models.db_storage import ImageStorage, RedisCache
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def post_user(user_id):
+    """
+    Creates a user
+    """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    if 'email' not in request.get_json():
+        abort(400, description="Missing email")
+   if 'name' not in request.get_json():
+        abort(400, description="Missing name")
+    if 'password' not in request.get_json():
+        abort(400, description="Missing password")
+
+    data = request.get_json()
+    instance = User(**data)
+    instance.save()
+    return make_response(jsonify(instance.to_dict()), 201)
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def put_user(user_id):
+    """
+    Updates a user
+    """
+    user = storage.get(User, user_id)
+
+    if not user:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    ignore = ['id', 'email', 'name']
+
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ignore:
+            setattr(user, key, value)
+    storage.save()
+    return make_response(jsonify(user.to_dict()), 200)
